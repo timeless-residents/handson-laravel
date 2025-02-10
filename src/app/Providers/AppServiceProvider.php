039e362be93a9f8e4ffb64d1c9af887e1248358b
Timeless Residents <business@satotakuya.jp>
2025-02-11 08:27:54 +0900

    fix it

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
+            URL::forceScheme('https');
+        }
     }
 }
